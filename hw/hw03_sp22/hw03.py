95fcdf3be3c226d3d7ce99419b5c69acce85a614
QR1126 <1193523825@qq.com>
2022-05-08 10:51:17 +0800

    hw03_sp22 done

---
+++ hw/hw03_sp22/hw03.py
@@ -0,0 +1,163 @@
+HW_SOURCE_FILE = __file__
+
+
+def num_eights(pos):
+    """Returns the number of times 8 appears as a digit of pos.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if pos < 10:
+        if pos == 8:
+            return 1
+        else:
+            return 0
+    return num_eights(pos % 10) + num_eights(pos // 10)
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    # def help_func(index, ret, step):
+    #     if index == n:
+    #         return ret
+    #     elif num_eights(index) > 0 or index % 8 == 0:
+    #         return help_func(index + 1, ret - step, -step)
+    #     else:
+    #         return help_func(index + 1, ret + step, step)
+    # return help_func(1, 1, 1)
+    # def help_next(idx, step):
+    #     if idx % 8 == 0 or num_eights(idx) > 0:
+    #         return -step
+    #     return step
+    #
+    # def help_pingpong(ret, i, step):
+    #     if i == n:
+    #         return ret
+    #     return help_pingpong(ret + step, i + 1, help_next(i + 1, step))
+    # return help_pingpong(1, 1, 1)
+    def help_func(index):
+        if index <= 8:
+            return index
+        return step(index) + help_func(index - 1)
+
+    def step(idx):
+        if idx < 8:
+            return 1
+        elif (idx - 1) % 8 == 0 or num_eights(idx - 1) > 0:
+            return -1 * step(idx - 1)
+        return step(idx-1)
+
+    return help_func(n)
+
+def get_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> get_larger_coin(1)
+    5
+    >>> get_larger_coin(5)
+    10
+    >>> get_larger_coin(10)
+    25
+    >>> get_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def get_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> get_smaller_coin(25)
+    10
+    >>> get_smaller_coin(10)
+    5
+    >>> get_smaller_coin(5)
+    1
+    >>> get_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+
+def count_coins(change):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def constrained_count(change, smallest_coin):
+        if change == 0:
+            return 1
+        if change < 0:
+            return 0
+        if smallest_coin == None:
+            return 0
+        without_coin = constrained_count(change, get_larger_coin(smallest_coin))
+        with_coin = constrained_count(change - smallest_coin, smallest_coin)
+        return without_coin + with_coin
+    return constrained_count(change, 1)
